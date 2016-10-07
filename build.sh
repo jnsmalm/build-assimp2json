d89b5a7aae439296c4d534bc41d8b16c411bea6d
Jens Malmborg <jnsmalm@gmail.com>
2016-10-07 21:03:38 +0200

    Added build for appveyor.

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@ git clone https://github.com/acgessler/assimp2json.git
 cd assimp2json
 git submodule init && git submodule update
 mkdir b && cd b
-cmake .. -DASSIMP_BUILD_TESTS=OFF -DBUILD_SHARED_LIBS=OFF && make
+cmake .. -DCMAKE_BUILD_TYPE=Release -DASSIMP_BUILD_TESTS=OFF -DBUILD_SHARED_LIBS=OFF && make
 cd .. && cd ..
 
 mkdir dist
 cp assimp2json/bin/assimp2json ./dist
 cp assimp2json/LICENSE ./dist
-cd dist && tar -zcvf ../assimp2json.tar.gz .
+cd dist && tar -zcvf ../assimp2json-v2.0.1-osx.tar.gz .
 cd ..
